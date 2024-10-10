115c2f47889bf36f5cba8c39900e2b1ea8ad762c
Sullivan Xiong <38967557+SullivanXiong@users.noreply.github.com>
2024-10-09 21:36:05 -0700

    removed old svelte folders

--- league-of-legends-optimizer/src/lib/Catalog/util.js
+++
@@ -1,8 +0,0 @@
-export function parseTeam(data) {
-  return {
-    name: data["Team Name"],
-    players: Object.entries(data)
-      .filter(([key, value]) => key.includes("Players") && value !== "")
-      .map(([key, value]) => ({ name: value })),
-  };
-}
